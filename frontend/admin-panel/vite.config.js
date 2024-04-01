9aa27b39bbbe77463e76c00792324fda58538e28
KV <sharnet32@proton.me>
2024-04-01 16:28:36 +0200

    update

--- frontend/admin-panel/vite.config.js
+++ frontend/admin-panel/vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from 'path';
 
-import dotenv from 'dotenv';
-
-dotenv.config({ path: './.env.dev' });
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
